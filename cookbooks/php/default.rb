65c309531b1fe7b5b9a2fdb1d4efc03983f7b4b4
菅澤要平 <y_sugasawa@metis.local>
2016-12-21 18:34:24 +0900

    add php

---
+++ cookbooks/php/default.rb
@@ -0,0 +1,16 @@
+execute 'install php' do
+  user 'root'
+  command <<"EOS"
+    yum -y install epel-release
+    rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+    yum -y install --enablerepo=remi,remi-php56 php php-devel php-mbstring php-pdo php-gd php-xml
+EOS
+end
+
+execute 'install composer' do
+  user 'root'
+  command <<"EOS"
+    curl -sS https://getcomposer.org/installer | php
+    mv composer.phar /usr/local/bin/composer
+EOS
+end
