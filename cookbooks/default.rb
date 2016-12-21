b7d67b820408ed692a0cf50a754654ab8ec1f02b
菅澤要平 <y_sugasawa@metis.local>
2016-12-21 18:33:40 +0900

    add default

---
+++ cookbooks/default.rb
@@ -0,0 +1,27 @@
+execute 'change localtime to JST' do
+  user 'root'
+  command <<"EOS"
+    cp -p /usr/share/zoneinfo/Japan /etc/localtime
+    echo 'ZONE="Asia/Tokyo"' > /etc/sysconfig/clock
+    echo 'UTC=false' >> /etc/sysconfig/clock
+EOS
+end
+
+execute 'export LANG ja_JP.UTF-8' do
+  user 'root'
+  command <<"EOS"
+    sed -i -e 's/LANG=.*/LANG="ja_JP\.UTF-8"/' /etc/sysconfig/i18n
+    source /etc/sysconfig/i18n
+EOS
+end
+
+user 'webservice' do
+  action :create
+  password "$6$0stfpFypdycg5WmA$CH8.m4E7/k8VbjMsbPI82qeEWUZEA.edIqpeWFAed/H.MBTjaTONEJXQcXFAcB0Bk0G1kZhTcYRfhJTL1hfF9/" #webservice!
+end
+
+execute 'groupinstall' do
+  command 'sudo yum groupinstall "Development Tools" -y'
+end
+
+include_recipe 'selinux::disabled'
