98760c32db7b3a6cd8df223b837d5d19f014fb3b
菅澤要平 <y_sugasawa@metis.local>
2017-01-18 23:24:24 +0900

    mysqlのバージョンを5.7に変更

--- cookbooks/mysql/default.rb
+++ cookbooks/mysql/default.rb
@@ -1,5 +1,5 @@
-package 'http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm' do
-  not_if 'rpm -q mysql-community-release-el6-5'
+package 'http://dev.mysql.com/get/mysql57-community-release-el6-7.noarch.rpm' do
+  not_if 'rpm -q mysql57-community-release-el6-7'
 end
 
 package 'mysql-community-server'
@@ -26,13 +26,13 @@ MYSQL_PASSWORD = 'AdminX'
 execute 'mysql_secure_installation' do
   user 'root'
   command <<"EOS"
-    mysql -u root -e "SET PASSWORD=PASSWORD('#{MYSQL_PASSWORD}');"
+    tmp_pass=$(grep 'temporary password' /var/log/mysqld.log | awk -F"root@localhost: " '{print $2}')
+    mysql -u root -p"$tmp_pass" --connect-expired-password -e "SET GLOBAL validate_password_length=4;SET GLOBAL validate_password_policy=LOW;SET PASSWORD FOR root@localhost=PASSWORD('#{MYSQL_PASSWORD}');"
     mysql -uroot -p#{MYSQL_PASSWORD} -e "DELETE FROM mysql.user WHERE User='';"
     mysql -uroot -p#{MYSQL_PASSWORD} -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
     mysql -uroot -p#{MYSQL_PASSWORD} -e "DROP DATABASE test;"
     mysql -uroot -p#{MYSQL_PASSWORD} -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'"
     mysql -uroot -p#{MYSQL_PASSWORD} -e "FLUSH PRIVILEGES;"
-    cp -p /etc/my.cnf /etc/my.cnf.org
 EOS
-  not_if "which mysql"
+  not_if "ls /etc/my.cnf.org"
 end
